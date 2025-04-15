6189c65783448d445affb219f4fb177838f31059
JennyLarsson89 <jempaz@hotmail.com>
2025-04-15 12:51:23 +0200

    changes in script.ts

--- dist/js/script.js
+++ dist/js/script.js
@@ -9,6 +9,29 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
 };
 import { fetchBooks } from "./api.js";
 import { handleSearch } from "./search.js";
+const mainPage = document.querySelector('.main-page');
+const overlay = document.querySelector('.overlay');
+const backButton = document.querySelector('.back-button');
+/////////
+const bookOne = document.querySelector('.cover-1');
+const bookTwo = document.querySelector('.cover-2');
+const bookThree = document.querySelector('.cover-3');
+const bookFour = document.querySelector('.cover-4');
+const bookFive = document.querySelector('.cover-5');
+const bookSix = document.querySelector('.cover-6');
+const bookSeven = document.querySelector('.cover-7');
+const bookEight = document.querySelector('.cover-8');
+/////////
+function showBookInfo() {
+    mainPage.classList.add('hidden');
+    overlay.classList.remove('hidden');
+    console.log('Book info shown');
+}
+function showMainPage() {
+    mainPage.classList.remove('hidden');
+    overlay.classList.add('hidden');
+    console.log('Main page shown');
+}
 function loadBooks() {
     return __awaiter(this, void 0, void 0, function* () {
         try {
@@ -32,3 +55,12 @@ function loadBooks() {
 }
 loadBooks();
 handleSearch();
+bookOne.addEventListener('click', showBookInfo);
+bookTwo.addEventListener('click', showBookInfo);
+bookThree.addEventListener('click', showBookInfo);
+bookFour.addEventListener('click', showBookInfo);
+bookFive.addEventListener('click', showBookInfo);
+bookSix.addEventListener('click', showBookInfo);
+bookSeven.addEventListener('click', showBookInfo);
+bookEight.addEventListener('click', showBookInfo);
+backButton.addEventListener('click', showMainPage);
--- src/script.ts
+++ src/script.ts
@@ -2,6 +2,52 @@
 import {fetchBooks} from "./api.js";
 import {searchBooks, handleSearch} from "./search.js";
 
+const mainPage : HTMLElement = document.querySelector('.main-page');
+const overlay : HTMLElement = document.querySelector('.overlay');
+const backButton : HTMLElement = document.querySelector('.back-button');
+/////////
+const bookOne = document.querySelector('.cover-1')
+const bookTwo = document.querySelector('.cover-2')
+const bookThree = document.querySelector('.cover-3')
+const bookFour = document.querySelector('.cover-4')
+const bookFive = document.querySelector('.cover-5')
+const bookSix = document.querySelector('.cover-6')
+const bookSeven = document.querySelector('.cover-7')
+const bookEight = document.querySelector('.cover-8')
+/////////
+
+function showBookInfo() {
+    mainPage.classList.add('hidden');
+    overlay.classList.remove('hidden');
+
+    console.log('Book info shown');
+}
+
+
+function showMainPage() {
+    mainPage.classList.remove('hidden');
+    overlay.classList.add('hidden');
+
+    console.log('Main page shown');
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 async function loadBooks() {
     try {
         const books = await fetchBooks();
@@ -23,3 +69,14 @@ async function loadBooks() {
 
 loadBooks();
 handleSearch();
+
+bookOne.addEventListener('click', showBookInfo)
+bookTwo.addEventListener('click', showBookInfo)
+bookThree.addEventListener('click', showBookInfo)
+bookFour.addEventListener('click', showBookInfo)
+bookFive.addEventListener('click', showBookInfo)
+bookSix.addEventListener('click', showBookInfo)
+bookSeven.addEventListener('click', showBookInfo)
+bookEight.addEventListener('click', showBookInfo)
+
+backButton.addEventListener('click', showMainPage);
